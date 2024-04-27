0eb2d22718e4226b99e5099ef02ecf69c04a8573
Jose Manuel Sánchez Báez <jmsanchezbz@uoc.edu>
2024-04-27 22:37:31 +0200

    Added Contest page + minor improvements

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,17 +3,25 @@
  * see commented examples below
  */
 
-
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
-import * as bootstrap from 'bootstrap';
+import * as bootstrap from "bootstrap";
 
 /**
  * Write any other JavaScript below
  */
 
-+( function() {
++(function () {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
-} )();
+})();
+
+const myCarouselElement = document.querySelector("#my-carousel");
+
+if (myCarouselElement) {
+  const carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: 4000,
+    touch: false,
+  });
+}
